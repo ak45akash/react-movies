e541795b36a351b5660e12edc5f7089a11394619
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-02-24 16:33:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'; //hook
+
+const App = () => {
+
+
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Hello World
+    </h1>
+  );
+}
+
+export default App
