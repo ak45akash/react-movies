55bc598743f8f4fb7b7a300c23554945ea3411fe
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-02-25 20:00:49 +0530

    Fetched the movie data from The Movie API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,92 @@
-import { useState, useEffect } from 'react'; //hook
+import { useState, useEffect } from 'react';
+import Search from './components/Search';
+import Spinner from './components/Spinner';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
 
 const App = () => {
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async() => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+
+      if (data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (e) {
+      console.error(`Error fetching movies: ${e}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World
-    </h1>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>
+              <span className='text-gradient'>
+              Find
+              </span> Movies You'll Enjoy Without the <span className="text-gradient">Hassle</span>
+            </h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
+
+          <section className='all-movies'>
+            <h2 className='mt-[40px]'>
+              All Movies
+            </h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  // <p key={movie.id} className='text-white'>{movie.title}</p>
+                  <li key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div className="">
+        <img src="search.svg" alt="search" />
+
+        <input 
+          type="text" 
+          placeholder="Search through thousands of movies" 
+          value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)} 
+          className=""
+         />
+      </div>
+    </div>
+  )
+}
+
+export default Search;
